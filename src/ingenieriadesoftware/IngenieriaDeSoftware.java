86351650ed336b42b015df5a6082e7c19ab0c4c5
jorge <jorge@192.168.1.9>
2018-08-29 18:51:00 -0300

    Primer hola mundo.

--- src/ingenieriadesoftware/IngenieriaDeSoftware.java
+++ src/ingenieriadesoftware/IngenieriaDeSoftware.java
@@ -16,6 +16,8 @@ public class IngenieriaDeSoftware {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.print("HOLA MUNDO");
     }
     
 }
