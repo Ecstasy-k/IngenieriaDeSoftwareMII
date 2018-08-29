fd0914c46fea6c5475f5229620815eafc367864b
Ecstasy-k <jorge.puente.08@gmail.com>
2018-08-29 18:54:24 -0300

    Agregando nombre al hola mundo
    Agregando nombre al hola mundo

--- src/ingenieriadesoftware/IngenieriaDeSoftware.java
+++ src/ingenieriadesoftware/IngenieriaDeSoftware.java
@@ -16,8 +16,9 @@ public class IngenieriaDeSoftware {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        public string nombre = Jorge;
+        System.out.print("HOLA " + nombre);
         
-        System.out.print("HOLA MUNDO");
     }
     
 }
